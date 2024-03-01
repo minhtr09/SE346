d60f768003f0b3f5d1a1b38cbb24a56fd9e9d83a
TriNguyen090102 <22521523@gm.uit.edu.vn>
2024-03-01 16:02:15 +0700

    start creen + metamask connect

--- src/screens/Homes/Game/Start/index.tsx
+++ src/screens/Homes/Game/Start/index.tsx
@@ -1,17 +1,55 @@
-import { View, Image, TouchableWithoutFeedback } from "react-native";
-
+import { View, Image, TouchableWithoutFeedback, TouchableOpacity, Text } from "react-native";
+import React, { useRef, useState, useEffect } from "react";
+import { WalletConnectModal, useWalletConnectModal } from '@walletconnect/modal-react-native'
 import LOGO from "../../../../assets/images/logo.png";
 import PLAY from "../../../../assets/images/play.png";
+import MESSAGE from "../../../../assets/images/message.png";
 
 import { styles } from "./styles";
 
+
+const projectId = '4e178defe3ed3ce84ed2b0c72f38aca8'
+const project_name = '4e178defe3ed3ce84ed2b0c72f38aca8'
+
+
+const providerMetadata = {
+    name: project_name,
+    description: 'YOUR_PROJECT_DESCRIPTION',
+    url: 'https://your-project-website.com/',
+    icons: ['https://your-project-logo.com/'],
+    redirect: {
+        native: 'YOUR_APP_SCHEME://',
+        universal: 'YOUR_APP_UNIVERSAL_LINK.com'
+    }
+}
+
+
 const Start = ({ handleOnStartGame }) => {
+    const [isGameOver, setIsGameOver] = useState(false);
+    const { isOpen, open, close, provider, isConnected, address } = useWalletConnectModal();
+    const handleWalletConnectButton = async () => {
+        if (isConnected) {
+            return provider?.disconnect();
+        }
+        return open();
+    }
+
+
     return (
         <View style={styles.container}>
-            <Image source={LOGO} style={styles.logo} />
-            <TouchableWithoutFeedback onPress={handleOnStartGame}>
-                <Image source={PLAY} style={styles.playButton} />
-            </TouchableWithoutFeedback>
+            <Image source={MESSAGE} style={styles.logo} />
+            {isConnected ? (
+                <TouchableWithoutFeedback onPress={handleOnStartGame}>
+                    <Image source={PLAY} style={styles.playButton} />
+                </TouchableWithoutFeedback>
+            ) : (
+                <TouchableOpacity style={styles.connectButton} onPress={handleWalletConnectButton}>
+                    <Text style={styles.monospaceText}>Connect Wallet</Text>
+                </TouchableOpacity>
+            )}
+
+            <WalletConnectModal projectId={projectId} providerMetadata={providerMetadata} />
+
         </View>
     );
 };
