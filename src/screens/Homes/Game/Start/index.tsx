fbe54c6dcbbb39931c6743ed546ebcf453b09c1a
ThInhHuynh <22521406@gm.uit.edu.vn>
2024-04-20 17:43:25 +0700

    installing lib

--- src/screens/Homes/Game/Start/index.tsx
+++ src/screens/Homes/Game/Start/index.tsx
@@ -3,15 +3,18 @@ import React from "react";
 
 import LOGO from "../../../../assets/images/logo.png";
 import PLAY from "../../../../assets/images/play.png";
+import STORE from "../../../../assets/images/store.png"
 
 import { styles } from "./styles";
 import { useAccount, useDisconnect } from "wagmi";
 import { useWeb3Modal } from "@web3modal/wagmi-react-native";
 import Button from "../../../../components/Button";
 import { useStateContext } from "../../../../context";
+import { useNavigation } from '@react-navigation/native'
 
 const Start = ({ handleOnStart }) => {
   const { isConnected, address } = useStateContext();
+  const navigation= useNavigation()
 
   const { open } = useWeb3Modal()
   const handleConnect = () => {
@@ -39,6 +42,9 @@ const Start = ({ handleOnStart }) => {
             <Image source={PLAY} style={styles.playButton} />
           </TouchableOpacity>
           <Button text="Disconnect" onPress={disconnect} />
+          {/* <TouchableOpacity onPress={()=>navigation.navigate('Store')}>
+            <Image source={STORE} style={styles.playButton} />
+          </TouchableOpacity> */}
         </>
 
       ) : (<Button text="Connect Wallet" onPress={handleConnect} />)}
