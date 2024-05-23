7d7d42311d2cc995b26be719a3c1322c794abe4f
trinhsang02 <22521243@gm.uit.edu.vn>
2024-05-23 08:44:19 +0700

    add firebase

--- src/screens/Homes/Game/index.tsx
+++ src/screens/Homes/Game/index.tsx
@@ -19,6 +19,10 @@ import Eight from "../../../assets/images/8.png";
 import Nine from "../../../assets/images/9.png";
 import Zero from "../../../assets/images/0.png";
 
+import { FIRESTORE_DB } from "../../../../firebaseConfig";
+import { addDoc, collection } from "firebase/firestore";
+
+
 const numberImages = {
   0: Zero,
   1: One,
@@ -36,32 +40,39 @@ const Game = () => {
   const [running, setIsRunning] = useState(false);
   const [isGameOver, setIsGameOver] = useState(false);
   const [currentPoints, setCurrentPoints] = useState(0);
+  const [scoreSaved, setScoreSaved] = useState(false);
 
   const gameEngineRef = useRef();
 
   const handleBackToStart = () => {
     setIsRunning(false);
     setIsGameOver(false);
+    setScoreSaved(false);
   };
 
   const handleOnStart = () => {
     setIsRunning(true);
     setIsGameOver(false);
+    setScoreSaved(false);
   };
 
-  const handleOnGameOver = () => {
+  const handleOnGameOver = async () => {
     setIsRunning(false);
     setIsGameOver(true);
+    
+    // await addScoreToFirebase(currentPoints); // Add this line to save the score to Firestore
+    // setCurrentPoints(0); // Reset points after saving
   };
 
   const handleOnEvent = (e) => {
     switch (e.type) {
       case "game_over":
-        handleOnGameOver();
-        setCurrentPoints(0);
+          handleOnGameOver();
+          //setCurrentPoints(0);
         break;
       case "new_point":
         setCurrentPoints(currentPoints + 1);
+        addScoreToFirebase(currentPoints);
         break;
     }
   };
@@ -78,6 +89,20 @@ const Game = () => {
           <Image source={numberImages[secondDigit]} style={{ width: 50, height: 70 }} />
         </View>
       );
+    }
+  };
+
+  // Add score to db
+  const addScoreToFirebase = async (score) => {
+    try {
+      const scoresCollection = collection(FIRESTORE_DB, "db_scores"); // Replace "scores" with your collection name
+      const newScoreDoc = await addDoc(scoresCollection, {
+        score, // Store the score in the document
+        // Can add other fields
+      });
+      console.log("Score added to Firestore with ID:", newScoreDoc.id);
+    } catch (error) {
+      console.error("Error adding score to Firestore:", error);
     }
   };
 
